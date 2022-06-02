cb3ac9069848b62d1fbd71d83df0b12dce35508e
Elijah Johnson <elijohnson565@gmail.com>
2022-06-02 16:51:51 -0700

    heroku fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/due', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://https://budget-tracker-final-project.herokuapp.com/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
