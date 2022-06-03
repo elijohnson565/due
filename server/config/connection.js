bc617812b30598d7c004ed0f654a09117d2c7b72
Elijah Johnson <elijohnson565@gmail.com>
2022-06-02 18:00:58 -0700

    heroku fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//fixing mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Bill-Tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
